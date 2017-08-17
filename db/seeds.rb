278430655fba33e6f8547f0aad6bf92ea85d3292
Cassidy Hales <cassidyhales11@gmail.com>
2017-08-17 10:48:49 -0600

    Implemented edit functionality for the status

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@ end
 
 puts "1 user created"
 
+1.times do |user|
+  AdminUser.create!(first_name: "Cassidy", last_name: "Hales", email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+end
+
+puts "1 Admin user created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rational: "#{post} rational content", user: User.first)
 end
